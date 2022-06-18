85989ab04378fea07cc272a6cfd271fe1cf86854
exeloz <christophercoulombe@gmail.com>
2022-06-18 09:32:29 -0400

    ray for test_player

--- test_player.py
+++ test_player.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import sys
+import ray
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -16,6 +17,9 @@ if __name__ == '__main__':
         num_cpus = int(sys.argv[1])
     else:
         num_cpus = 4
+
+    ray.init(num_cpus=num_cpus)
+
     n_players = 4
     config_filename = f'configs/config-{n_players}-players'
     evaluator = ZombieEvaluator(4, 5000, 40000, verbose=True)
@@ -23,3 +27,5 @@ if __name__ == '__main__':
     to_load = [f'to_load/{f}' for f in listdir('to_load/') if isfile(join('to_load/', f))]
     genomes = [ZombieEvaluator.load_genome(g) for g in to_load]
     evaluator.eval_genomes(genomes, ZombieEvolver.load_config(config_filename))
+    ray.shutdown()
+
